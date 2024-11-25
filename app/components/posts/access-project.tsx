a53ac6ab00506eec88f3f90880c4e5e1a86fc32b
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-11-25 13:14:29 -0300

    feat: disable project link when deploy URL is not available

--- app/components/posts/access-project.tsx
+++ app/components/posts/access-project.tsx
@@ -1,7 +1,7 @@
 import { GlobeIcon } from "lucide-react";
 
 const AccessProject = ({ deploy }: { deploy: string }) => {
-  return (
+  return deploy ? (
     <a
       href={deploy}
       target="_blank"
@@ -11,6 +11,11 @@ const AccessProject = ({ deploy }: { deploy: string }) => {
       Ver projeto
       <GlobeIcon size={14} />
     </a>
+  ) : (
+    <span className="flex w-full cursor-not-allowed items-center justify-between text-nowrap rounded-xl bg-background px-5 py-2.5 text-sm uppercase">
+      Ver projeto
+      <GlobeIcon size={14} />
+    </span>
   );
 };
 
