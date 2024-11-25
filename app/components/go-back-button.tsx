f555c7c65b08fc98cc2bdddf296b100443136daf
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-11-24 23:20:54 -0300

    refactor: replace dynamic go-back functionality with a static home link

--- app/components/go-back-button.tsx
+++ app/components/go-back-button.tsx
@@ -1,27 +1,17 @@
-"use client";
+import Link from "next/link";
 
-import { useRouter } from "next/navigation";
 import { ArrowLeftIcon } from "lucide-react";
 
 const GoBackButton = () => {
-  const router = useRouter();
-
-  const handleGoBack = () => {
-    if (window.history.length > 1) {
-      router.back();
-    } else {
-      router.push("/");
-    }
-  };
-
   return (
-    <button
-      onClick={handleGoBack}
-      className="flex items-center gap-2.5 rounded-xl bg-background px-3 py-1.5 text-sm uppercase"
+    <Link
+      href="/"
+      prefetch
+      className="flex w-fit items-center gap-2.5 rounded-xl bg-background px-3 py-1.5 text-sm uppercase"
     >
       <ArrowLeftIcon size={14} />
       Voltar
-    </button>
+    </Link>
   );
 };
 
